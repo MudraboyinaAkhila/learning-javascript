ab350acde3ed94cdacc6e4773e004cc11e8d9834
MudraboyinaAakhila <mudraboinaakhila43@gmail.com>
2026-04-11 09:27:45 +0530

    files addede

---
+++ day2/script.js
@@ -0,0 +1,25 @@
+// console.log(-1)
+
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// const b = 54;
+// console.log(b);
+
+// // we want both means we use "let"
+
+// let c = 32;
+// c = 64;
+// console.log(c);
+
+// Arrays
+
+let userData = [10, "hello", 3.42, null, 'bye'];
+
+// console.log(userData[0]);
+
+for(let index = 0; index < userData.length; index++) {
+    console.log(typeof userData[index]);
+
+}
